ae345f9f1cbea90e79e89b695424d8b5251d4c2a
chumpon-asaneerat <chumponsenate@gmail.com>
2023-02-28 10:03:54 +0700

    Implements #37 - Implements NLib.Serial.Devices project #13.

---
+++ 09.App/NLib.Serial.Terminal.App/Controls/SerialCommSetting.xaml.cs
@@ -0,0 +1,85 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NLib.Serial;
+using NLib.Serial.Devices;
+using NLib.Serial.Terminals;
+
+#endregion
+
+namespace NLib.Serial.Terminal.App.Controls
+{
+    /// <summary>
+    /// Interaction logic for SerialCommSetting.xaml
+    /// </summary>
+    public partial class SerialCommSetting : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SerialCommSetting()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        private SerialDevice _device = null;
+
+        #endregion
+
+        #region Private Methods
+
+        private void EnableInputs(bool enabled)
+        {
+            cbPortNames.IsEnabled = enabled;
+            txtBoadRate.IsEnabled = enabled;
+            cbParities.IsEnabled = enabled;
+            txtDataBit.IsEnabled = enabled;
+            cbStopBits.IsEnabled = enabled;
+            cbHandshakes.IsEnabled = enabled;
+        }
+
+        private void InitControls()
+        {
+            if (null == _device)
+            {
+                EnableInputs(false);
+                return;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Setup.
+        /// </summary>
+        /// <param name="device">The device.</param>
+        public void Setup(SerialDevice device)
+        {
+            _device = device;
+            InitControls();
+        }
+
+        #endregion
+    }
+}
