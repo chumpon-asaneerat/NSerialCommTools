473095a59a2946aa5867190272abb56248a37de9
chumpon-asaneerat <chumponsenate@gmail.com>
2023-02-28 10:39:48 +0700

    Implements #44 - Implements NLib.Serial.Devices project #19.

---
+++ 09.App/NLib.Serial.Terminal.App/Controls/TFO1TerminalPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NLib.Serial.Terminal.App.Controls
+{
+    /// <summary>
+    /// Interaction logic for TFO1TerminalPage.xaml
+    /// </summary>
+    public partial class TFO1TerminalPage : UserControl
+    {
+        public TFO1TerminalPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
