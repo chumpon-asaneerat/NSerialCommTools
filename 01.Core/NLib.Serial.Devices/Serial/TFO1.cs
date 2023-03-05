59ad83aefb9045b2fd49d21f3c48319132cdde7a
chumpon-asaneerat <chumponsenate@gmail.com>
2023-03-05 18:32:25 +0700

    Implements #62 - Update projects structures #8.

--- 01.Core/NLib.Serial.Devices/Serial/TFO1.cs
+++ 01.Core/NLib.Serial.Devices/Serial/TFO1.cs
@@ -281,3 +281,135 @@ namespace NLib.Serial.Devices
 
     #endregion
 }
+
+namespace NLib.Serial.Emulators
+{
+    #region TFO1Device
+
+    /// <summary>
+    /// The TFO1Device class.
+    /// </summary>
+    public class TFO1Device : SerialDeviceEmulator<TFO1Data>
+    {
+        #region Singelton Access
+
+        private static TFO1Device _instance = null;
+        /// <summary>
+        /// Singelton Access.
+        /// </summary>
+        public static TFO1Device Instance
+        {
+            get
+            {
+                if (null == _instance)
+                {
+                    lock (typeof(TFO1Device))
+                    {
+                        _instance = new TFO1Device();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        private TFO1Device()
+        {
+        }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~TFO1Device()
+        {
+            Shutdown();
+        }
+
+        #endregion
+
+        #region Onverride method(s)
+
+        /// <summary>
+        /// ProcessRXQueue.
+        /// </summary>
+        protected override void ProcessRXQueue()
+        {
+
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
+
+namespace NLib.Serial.Terminals
+{
+    #region TFO1Terminal
+
+    /// <summary>
+    /// The TFO1Terminal class.
+    /// </summary>
+    public class TFO1Terminal : SerialDeviceTerminal<TFO1Data>
+    {
+        #region Singelton Access
+
+        private static TFO1Terminal _instance = null;
+        /// <summary>
+        /// Singelton Access.
+        /// </summary>
+        public static TFO1Terminal Instance
+        {
+            get
+            {
+                if (null == _instance)
+                {
+                    lock (typeof(TFO1Terminal))
+                    {
+                        _instance = new TFO1Terminal();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        private TFO1Terminal()
+        {
+        }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~TFO1Terminal()
+        {
+            Disconnect();
+        }
+
+        #endregion
+
+        #region Onverride method(s)
+
+        /// <summary>
+        /// ProcessRXQueue.
+        /// </summary>
+        protected override void ProcessRXQueue()
+        {
+
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
